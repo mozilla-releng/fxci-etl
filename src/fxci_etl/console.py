c64d75013af7599d64c0c0c5ef6600af3cae12d6
Andrew Halberstadt <ahal@pm.me>
2024-04-30 10:39:37 -0400

    fix: pass user and password to 'listen()'

--- src/fxci_etl/console.py
+++ src/fxci_etl/console.py
@@ -5,11 +5,11 @@ import sys
 from fxci_etl.pulse import listen
 
 
-def run(args=sys.argv[1:]):
+def run(args: list[str] = sys.argv[1:]):
     parser = argparse.ArgumentParser()
     parser.add_argument("--user", help="PulseGuardian username")
     parser.add_argument("--password", help="PulseGuardian password")
 
     args = parser.parse_args(args)
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(listen())
+    loop.run_until_complete(listen(args.user, args.password))
